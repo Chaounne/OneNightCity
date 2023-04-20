1c8b0a18461145b0ed9db86381bbacf2933bf36a
Cédric-Alexandre Pascal <cedricalexandrepascal@gmail.com>
2023-04-20 11:06:59 +0200

    Update OneNightCity.java

--- src/main/java/me/chaounne/onenightcity/OneNightCity.java
+++ src/main/java/me/chaounne/onenightcity/OneNightCity.java
@@ -3,15 +3,10 @@ package me.chaounne.onenightcity;
 import me.chaounne.onenightcity.commands.Commands;
 import me.chaounne.onenightcity.events.Handler;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public final class OneNightCity extends JavaPlugin {
 
