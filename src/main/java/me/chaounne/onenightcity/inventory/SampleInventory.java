4aebb96ee0729df33c516b7bee4933400a6d8176
Cédric-Alexandre Pascal <cedricalexandrepascal@gmail.com>
2024-02-22 13:38:03 +0100

    trade tous les items d'un coup

--- src/main/java/me/chaounne/onenightcity/inventory/SampleInventory.java
+++ src/main/java/me/chaounne/onenightcity/inventory/SampleInventory.java
@@ -2,9 +2,9 @@ package me.chaounne.onenightcity.inventory;
 
 import me.chaounne.fastinv.FastInv;
 import me.chaounne.onenightcity.game.GamePlayer;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.ChatColor;
 
 public class SampleInventory extends FastInv {
 
@@ -38,32 +38,44 @@ public class SampleInventory extends FastInv {
             "Espece de rat, t'as que %d poudre(s) en echange pour la peine ! ",
             "Oula, c'est peu quand meme : %d poudre(s) ajoutées ! "
     };
-    public void trade(int amountPoudre, ItemStack given, Player player) {
-        if (player.getInventory().containsAtLeast(given, given.getAmount())) {
-            GamePlayer gamePlayer = GamePlayer.getInstance(player);
-            player.getInventory().removeItem(given);
 
-            gamePlayer.addScore(amountPoudre);
-            gamePlayer.getTeam().addScore(amountPoudre);
-            System.out.println(gamePlayer.getScore());
+    public void trade(int amountPoudre, ItemStack given, Player player){
+        if(player.getInventory().containsAtLeast(given, given.getAmount())){
+            GamePlayer gamePlayer = GamePlayer.getInstance(player);
+            //player.sendMessage(given+""+given.getAmount());
+            int amountEchange = 0;
+            int nbitem = 0;
+            for (ItemStack item : player.getInventory().getContents()) {
+                if (item != null && item.getType() == given.getType()) {
+                    nbitem = nbitem + item.getAmount();
+                }
+            }
+
+            while (nbitem >= given.getAmount()) {
+                player.getInventory().removeItem(given);
+                gamePlayer.addScore(amountPoudre);
+                gamePlayer.getTeam().addScore(amountPoudre);
+                amountEchange = amountEchange + amountPoudre;
+                nbitem = nbitem - given.getAmount();
+            }
+
 
             // Choix aléatoire de l'index du message
             int randomIndex = (int) (Math.random() * messages.length);
             // Choix aléatoire de la couleur
             ChatColor randomColor = colors[(int) (Math.random() * colors.length)];
             // Sélection du message aléatoire
-            String randomMessage = String.format(messages[randomIndex], amountPoudre);
+            String randomMessage = String.format(messages[randomIndex], amountEchange);
 
             // Envoi du message avec la couleur et le message aléatoire
             player.sendMessage(randomColor + randomMessage);
-
         }
+
     }
+
     public void addItem(ItemStack item, int slot, int amountPoudre){
         setItem(slot, item, e -> {
-            // Créez une copie de l'ItemStack avec la quantité spécifiée
             ItemStack itemStack = new ItemStack(item.getType(), item.getAmount());
-            // Passez l'ItemStack correct à la méthode trade
             trade(amountPoudre, itemStack, (Player) e.getWhoClicked());
         });
     }
