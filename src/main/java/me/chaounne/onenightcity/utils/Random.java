d54f9c9121c3c8832ff6e342ed82b09ee8fa24b6
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2024-03-19 10:16:01 +0100

    ajout javadoc pour random

--- src/main/java/me/chaounne/onenightcity/utils/Random.java
+++ src/main/java/me/chaounne/onenightcity/utils/Random.java
@@ -2,6 +2,11 @@ package me.chaounne.onenightcity.utils;
 
 public class Random {
 
+    /**
+     * @param min minimum
+     * @param max maximum
+     * @return a pseudorandom integer between min (inclusive) and max (inclusive)
+     */
     public static int between(int min, int max) {
         return (int) (Math.random() * (max - min + 1)) + min;
     }
